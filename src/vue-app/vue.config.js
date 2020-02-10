9a71d5c0f8dd3f8d1a21177fed939860e0984533
Yankovskiy Georgiy <52546287+yageorgiy@users.noreply.github.com>
2020-02-10 15:29:09 +0300

    Хот-фикс: фронтэнд приложение собиралось в другую папку.

--- src/vue-app/vue.config.js
+++ src/vue-app/vue.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "../../dist/"),
+  outputDir: path.resolve(__dirname, "../../dist/public_html"),
   lintOnSave: false
 };
